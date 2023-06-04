6db312f89f5f9ee4b64cd96b6e7b752a48eb6900
Eugene T <102475316+jonotyan@users.noreply.github.com>
2023-06-05 02:05:00 +0600

    added comment in loader.py
    added comment to line in loader.py

--- loader.py
+++ loader.py
@@ -4,5 +4,5 @@ from data import config
 
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = MemoryStorage()
+storage = MemoryStorage() #memory
 dp = Dispatcher(bot, storage=storage)
