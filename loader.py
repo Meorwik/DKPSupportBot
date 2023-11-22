c33523b10a8245954b3e9947bf880240ebb270f9
Meorwik <104008026+Meorwik@users.noreply.github.com>
2023-11-22 17:45:46 +0600

    test current timezone

--- loader.py
+++ loader.py
@@ -7,6 +7,7 @@ from data import config
 
 
 scheduler = AsyncIOScheduler()
+print(scheduler.timezone)
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
